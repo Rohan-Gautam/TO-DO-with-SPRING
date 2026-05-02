8f389236e6be82bd479f903352bdf1ac1a21848a
Name <136186521+Rohan-Gautam@users.noreply.github.com>
2026-05-02 16:46:53 +0530

    feat: add TodoRepository using JpaRepository for CRUD support

---
+++ src/main/java/com/example/TO_DO/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.example.TO_DO.repository;
+
+import com.example.TO_DO.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
