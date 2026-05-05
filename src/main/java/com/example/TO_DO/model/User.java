8957ba0ad344b0f96e418bdfd28e9f891aaf52d4
Name <136186521+Rohan-Gautam@users.noreply.github.com>
2026-05-06 00:43:59 +0530

    feat: added user entity, UserRepository

--- src/main/java/com/example/TO_DO/model/User.java
+++ src/main/java/com/example/TO_DO/model/User.java
@@ -1,4 +1,54 @@
 package com.example.TO_DO.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.PrivateKey;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role = "ROLE_USER";
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
