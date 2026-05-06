976d559522c4d38fa0f447e6a22c72815d295738
Name <136186521+Rohan-Gautam@users.noreply.github.com>
2026-05-06 15:23:01 +0530

    feat: added JwtUtil

---
+++ src/main/java/com/example/TO_DO/security/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.example.TO_DO.security;
+
+import com.example.TO_DO.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "your-super-secret-key-that-is-long-enough-for-hs256-algorithm";
+    private final long EXPIRATION = 1000 * 60 * 60 * 24; // 24 hours in milliseconds
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("id", user.getId())
+                .claim("name", user.getName())
+                .claim("role", user.getRole())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        String email = extractEmail(token);
+        boolean isExpired = extractAllClaims(token).getExpiration().before(new Date());
+        return email.equals(user.getEmail()) && !isExpired;
+    }
+}
