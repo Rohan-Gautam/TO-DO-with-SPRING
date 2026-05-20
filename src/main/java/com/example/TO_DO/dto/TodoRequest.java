4c0e3a500066d2d8c0f0639e579112ec1e3389f9
Name <136186521+Rohan-Gautam@users.noreply.github.com>
2026-05-20 14:27:21 +0530

    feat: implement validation to check title, description and completed

--- src/main/java/com/example/TO_DO/dto/TodoRequest.java
+++ src/main/java/com/example/TO_DO/dto/TodoRequest.java
@@ -1,10 +1,18 @@
 package com.example.TO_DO.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class TodoRequest {
+
+    @NotBlank(message = "Title cannot be empty")
+    @Size(min = 1, max = 100, message = "Title must be between 1 and 100 characters")
     private String title;
+
+    @Size(max = 1000, message = "Description cannot exceed 1000 characters")
     private String description;
+
     private boolean completed;
 }
