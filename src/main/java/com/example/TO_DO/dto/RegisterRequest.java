499424437e4e4b0936cbaca40829b8c539d1ce3e
Name <136186521+Rohan-Gautam@users.noreply.github.com>
2026-05-20 14:26:52 +0530

    feat: implement validation to check name, email and password

--- src/main/java/com/example/TO_DO/dto/RegisterRequest.java
+++ src/main/java/com/example/TO_DO/dto/RegisterRequest.java
@@ -1,10 +1,22 @@
 package com.example.TO_DO.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+
+    @NotBlank(message = "Name cannot be empty")
     private String name;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            message = "Please provide a valid email")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(max = 32, min = 6, message = "Password must be at least 6 characters and less than 32 characters")
     private String password;
 }
